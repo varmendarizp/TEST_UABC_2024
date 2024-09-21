28d05f9b278724b2f4d7ed9f91f753f9852813a0
varmendarizp <valeria.armendariz.pardo@uabc.edu.mx>
2024-09-21 14:30:42 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,12 +16,33 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    reg [23:0] counter;
+    reg [3:0] display_value;
+    wire [6:0] segment_reg;
+    assign uo_out[6:0] = segment_reg;
+    assign uo_out[7] = 1'b0;
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            counter <= 24'd0;
+            display_value <= 4'd0;
+        end else if (counter == 24'd10000000) begin
+            counter <= 24'd0;
+            display_value <= display value + 1'b1;
+
+            if (display_value == 4'd15) begin
+                display_value <= 4'd0;
+            end
+        end else begin
+            counter <= counter + 1'b1;
+        end 
+    end
+    
+    seg7 seg7(.digit(display_value), .segments(segment_reg));
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena,ui_in,uio_in};
 
 endmodule
